db28d4ca7a5a7086db18686f1a55160f020140a9
AtryzA <umwdream2017@gmail.com>
2021-08-18 15:49:25 +0900

    PreRefactor

--- Nocca_AI.py
+++ Nocca_AI.py
@@ -1,7 +1,7 @@
 class Nocca_AI(object):
 
-    def select_stone(self, board: list[list[list]], stone: list[tuple[int,int]]) -> int:
-        return map(int,stone[0])
+    def select_stone(self, board: list[list[list]], stone: list[int]) -> int:
+        return stone[0]
 
-    def select_direction(self, direction: list)-> str:
-        return str(7)
+    def select_direction(self, direction: list)-> int:
+        return 7
