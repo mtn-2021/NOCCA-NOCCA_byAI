f44ef8468061ff623174101fd0656555aac4eff8
hidenaka824 <hidenaka824@yahoo.co.jp>
2021-08-19 01:12:59 +0900

    Fixed evaluateState.py

--- evaluateState.py
+++ evaluateState.py
@@ -9,7 +9,7 @@ def evaluateState(states):
       isNearbyE=0 #AIから見てUserの駒が近くにあるか
       isNearbyC=0 #Userから見てAIの駒が近くにあるか
 
-      
+
       #葉を探すために子供の数（childNum）を見ていく
       #childNumが0だったらそれは葉である
       #値が入っていない(Noneのとき)も考慮して条件式を書いた
@@ -18,192 +18,181 @@ def evaluateState(states):
         #######駒の位置について考える###########
         #AIの駒の座標(E_position)を評価する
         for position in Node.state.E_position:
-                      
-            positionEmP=position#AIの駒の位置を代入(高さなし)
+            positionEmP=position%100#AIの駒の位置を代入(高さなし)
 
-            #高さの情報をなくして判断（平面で考える）
-            if (200<=positionEmP):
-              positionEmP=positionEmP-200
-
-            if (100<=positionEmP):
-              positionEmP=positionEmP-100
-
-            #座標1から5までなら0点
+            #座標0から4までなら1点
             #以下同
-            if 1<=positionEmP and positionEmP<=5:
-             positionPointE=positionPointE+1
+            if 0<=positionEmP and positionEmP<=4:
+                positionPointE=positionPointE+1
 
-            elif 6<=positionEmP and positionEmP<=10:
-              positionPointE=positionPointE+2
+            elif 5<=positionEmP and positionEmP<=9:
+                positionPointE=positionPointE+2
 
-            elif 11<=positionEmP and positionEmP<=15:
-              positionPointE=positionPointE+3
-          
-            elif 16<=positionEmP and positionEmP<=20:
-              positionPointE=positionPointE+4
-          
-            elif 21<=positionEmP and positionEmP<=25:
-              positionPointE=positionPointE+5
-          
-            elif 26<=positionEmP and positionEmP<=30:
-              positionPointE=positionPointE+6
+            elif 10<=positionEmP and positionEmP<=14:
+                positionPointE=positionPointE+3
+
+            elif 15<=positionEmP and positionEmP<=19:
+                positionPointE=positionPointE+4
+
+            elif 20<=positionEmP and positionEmP<=24:
+                positionPointE=positionPointE+5
+
+            elif 25<=positionEmP and positionEmP<=29:
+                positionPointE=positionPointE+6
 
             #マスの端っこは点数が低い
-            if (positionEmP==1,6,11,16,21,26,5,10,15,20,25,30):
-              positionPointE=positionPointE-1
+            if (positionEmP==0 or
+                positionEmP==4 or
+                positionEmP==5 or
+                positionEmP==9 or
+                positionEmP==10 or
+                positionEmP==14 or
+                positionEmP==15 or
+                positionEmP==19 or
+                positionEmP==20 or
+                positionEmP==24 or
+                positionEmP==25 or
+                positionEmP==29 ):
+                positionPointE=positionPointE-2
 
         #Userの駒の座標(C_position)を評価する
         for position in Node.state.P_position:
-
-          positionCmP=position#Userの駒の位置を代入
-
-          #高さの情報をなくして判断（平面で考える）
-          if 200<=positionCmP:
-            positionCmP=positionCmP-200
-
-          if 100<=positionCmP:
-            positionCmP=positionCmP-100
+            positionCmP=position%100#Userの駒の位置を代入(高さなし)
 
 
-          if 1<=positionCmP and positionCmP<=5:
-               positionPointC=positionPointC+6
+            if 0<=positionCmP and positionCmP<=4:
+                positionPointC=positionPointC+6
 
-          elif 6<=positionCmP and positionCmP<=10:
-               positionPointC=positionPointC+5
+            elif 5<=positionCmP and positionCmP<=9:
+                positionPointC=positionPointC+5
 
-          elif 11<=positionCmP and positionCmP<=15:
-               positionPointC=positionPointC+4
-          
-          elif 16<=positionCmP and positionCmP<=20:
-               positionPointC=positionPointC+3
-          
-          elif 21<=positionCmP and positionCmP<=25:
-               positionPointC=positionPointC+2
-          
-          elif 26<=positionCmP and positionCmP<=30:
-               positionPointC=positionPointC+1
-          
-          #マスの端っこは点数が低い
-          if (positionCmP==1,6,11,16,21,26,5,10,15,20,25,30):
-               positionPointC=positionPointC-1
+            elif 10<=positionCmP and positionCmP<=14:
+                positionPointC=positionPointC+4
+
+            elif 15<=positionCmP and positionCmP<=19:
+                positionPointC=positionPointC+3
+
+            elif 20<=positionCmP and positionCmP<=24:
+                positionPointC=positionPointC+2
+
+            elif 25<=positionCmP and positionCmP<=29:
+                positionPointC=positionPointC+1
+
+            #マスの端っこは点数が低い
+            if (positionCmP==0 or
+                positionCmP==4 or
+                positionCmP==5 or
+                positionCmP==9 or
+                positionCmP==10 or
+                positionCmP==14 or
+                positionCmP==15 or
+                positionCmP==19 or
+                positionCmP==20 or
+                positionCmP==24 or
+                positionCmP==25 or
+                positionCmP==29):
+                positionPointC=positionPointC-2
+
+
+
 
 
 
         ########相手の駒の周りについて考える##########
         #AIの駒から見てUserの駒が近くにあるか確認
         for positionE in Node.state.E_position:
-          for positionC in Node.state.P_position:
-            #print("AI:",positionE)
-            #print("User",positionC)
-            positionEm=positionE#AIの駒の位置を代入
-            positionCm=positionC#Userの駒の位置を代入
+            for positionC in Node.state.P_position:
+                #print("AI:",positionE)
+                #print("User",positionC)
+                positionEm=positionE%100#AIの駒の位置を代入（高さなし）
+                positionCm=positionC%100#Userの駒の位置を代入（高さなし）
+
+                #近くにUserの駒があるか確認
+                #あったら減点する
+                #探索木が1個目か3個目の時
+                if Node.depth%2==1:
+                    if (positionEm==positionCm-6 or
+                    positionEm==positionCm-5 or
+                    positionEm==positionCm-4 or
+                    positionEm==positionCm-1 or
+                    positionEm==positionCm+1 or
+                    positionEm==positionCm+4 or
+                    positionEm==positionCm+5 or
+                    positionEm==positionCm+6 ):
+                        isNearbyE=isNearbyE-2
+
+                #探索木が2個目か4個目の時
+                if Node.depth%2==0:
+                    #近くにUserの駒があるか確認
+                    #あったら加点する
+                    if (positionEm==positionCm-6 or
+                    positionEm==positionCm-5 or
+                    positionEm==positionCm-4 or
+                    positionEm==positionCm-1 or
+                    positionEm==positionCm+1 or
+                    positionEm==positionCm+4 or
+                    positionEm==positionCm+5 or
+                    positionEm==positionCm+6 ):
+                        isNearbyE=isNearbyE+2
 
 
-            #高さの情報をなくして判断（平面で考える）
-            if 200<=positionEm:
-              positionEm=positionEm-200
+                #Userの駒に乗ってたら加点する
+                if positionE+100==positionC:
+                    isNearbyE=isNearbyE+10
 
-            if 100<=positionEm:
-              positionEm=positionEm-100
+                #仲間と同じとこに行くと点数が低くなる
+                for positionEE in Node.state.E_position:
+                    if positionE+100==positionEE:
+                        isNearbyE=isNearbyE-1
 
-            if 200<=positionCm:
-              positionCm=positionCm-200
-
-            if 100<=positionCm:
-              positionCm=positionCm-100
-
-            #探索木が1個目か3個目か5個目のとき  
-            if Node.depth%2==1:
-              #近くにUserの駒があるか確認
-              #あったら-1する
-              if (positionEm==positionCm-6 or
-                positionEm==positionCm-5 or
-                positionEm==positionCm-4 or 
-                positionEm==positionCm-1 or 
-                positionEm==positionCm+1 or 
-                positionEm==positionCm+4 or 
-                positionEm==positionCm+5 or 
-                positionEm==positionCm+6 ):
-                isNearbyE=isNearbyE-1
-
-            #探索木が2個目か4個目の時
-            if Node.depth%2==0:
-              #近くにUserの駒があるか確認
-              #あったら+1する
-              if (positionEm==positionCm-6 or
-                positionEm==positionCm-5 or
-                positionEm==positionCm-4 or 
-                positionEm==positionCm-1 or 
-                positionEm==positionCm+1 or 
-                positionEm==positionCm+4 or 
-                positionEm==positionCm+5 or 
-                positionEm==positionCm+6 ):
-                isNearbyE=isNearbyE+1
-              
-
-            #Userの駒に乗ってたら+2する
-            if positionEm==positionCm and positionC<positionE:
-              isNearbyE=isNearbyE+2
 
             #print(isNearbyE)
 
         #Userの駒から見てAIの駒が近くにあるか確認
         for positionC in Node.state.P_position:
-          for positionE in Node.state.E_position:
+            for positionE in Node.state.E_position:
 
-            
-            positionEm=positionE#AIの駒の位置を代入
-            positionCm=positionC#Userから見てAIの駒が近くにあるか
+                positionEm=positionE%100#AIの駒の位置を代入（高さなし）
+                positionCm=positionC%100#Userの駒の位置を代入（高さなし）
 
-            #高さの情報をなくす（平面で考える）
-            if 200<=positionEm:
-              positionEm=positionEm-200
+                #探索木が1個目か3個目か5個目のとき
+                if Node.depth%2==1:
+                    #近くにAIの駒があるか確認
+                    #あったら加点する
+                    if (positionCm==positionEm-6 or
+                    positionCm==positionEm-5 or
+                    positionCm==positionEm-4 or
+                    positionCm==positionEm-1 or
+                    positionCm==positionEm+1 or
+                    positionCm==positionEm+4 or
+                    positionCm==positionEm+5 or
+                    positionCm==positionEm+6 ):
+                        isNearbyC=isNearbyC+2
 
-            if 100<=positionEm:
-              positionEm=positionEm-100
+                #探索木が2個目か4個目のとき
+                if Node.depth%2==0 :
+                    #近くにAIの駒があるか確認
+                    #あったら減点する
+                    if (positionCm==positionEm-6 or
+                    positionCm==positionEm-5 or
+                    positionCm==positionEm-4 or
+                    positionCm==positionEm-1 or
+                    positionCm==positionEm+1 or
+                    positionCm==positionEm+4 or
+                    positionCm==positionEm+5 or
+                    positionCm==positionEm+6 ):
+                        isNearbyC=isNearbyC-2
 
-            if 200<=positionCm:
-              positionCm=positionCm-200
-
-            if 100<=positionCm:
-              positionCm=positionCm-100
-
-            #探索木が1個目か3個目か5個目のとき
-            if Node.depth%2==1:
-              #近くにAIの駒があるか確認
-              #あったら+1する
-              if (positionCm==positionEm-6 or 
-                 positionCm==positionEm-5 or 
-                 positionCm==positionEm-4 or 
-                 positionCm==positionEm-1 or
-                 positionCm==positionEm+1 or 
-                 positionCm==positionEm+4 or 
-                 positionCm==positionEm+5 or 
-                 positionCm==positionEm+6 ):
-                isNearbyC=isNearbyC+1
-
-            #探索木が2個目か4個目のとき
-            if Node.depth%2==1:
-              #近くにAIの駒があるか確認
-              #あったら-1する
-              if (positionCm==positionEm-6 or 
-                 positionCm==positionEm-5 or 
-                 positionCm==positionEm-4 or 
-                 positionCm==positionEm-1 or
-                 positionCm==positionEm+1 or 
-                 positionCm==positionEm+4 or 
-                 positionCm==positionEm+5 or 
-                 positionCm==positionEm+6 ):
-                isNearbyC=isNearbyC-1
-
-             #AIの駒に乗ってたら+2する
-            if positionCm==positionEm and positionE<positionC:
-              isNearbyC=isNearbyC+2
-
-
+                #AIの駒に乗ってたら加点する
+                if positionC+100==positionE:
+                    isNearbyC=isNearbyC+10
 
 
         #今までの得点を合計してeval(評価点)に代入
-        Node.eval=positionPointE+isNearbyE-(positionPointC+isNearbyC)
+        if Node.depth%2==1 :
+            Node.eval=positionPointE+isNearbyE-(positionPointC+isNearbyC)
+
+        if Node.depth%2==0:
+            Node.eval=-(positionPointE+isNearbyE)+(positionPointC+isNearbyC)
 
     return states
