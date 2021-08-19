4570a8b6e6b5fc74ce8e72db23b323d38fc309f0
hidenaka824 <hidenaka824@yahoo.co.jp>
2021-08-19 12:04:54 +0900

    Fixed evaluateState.py

--- evaluateState.py
+++ evaluateState.py
@@ -23,22 +23,25 @@ def evaluateState(states):
             #座標0から4までなら1点
             #以下同
             if 0<=positionEmP and positionEmP<=4:
-                positionPointE=positionPointE+1
+                positionPointE=positionPointE+0
 
             elif 5<=positionEmP and positionEmP<=9:
-                positionPointE=positionPointE+2
+                positionPointE=positionPointE+1
 
             elif 10<=positionEmP and positionEmP<=14:
                 positionPointE=positionPointE+3
 
             elif 15<=positionEmP and positionEmP<=19:
-                positionPointE=positionPointE+4
+                positionPointE=positionPointE+6
 
             elif 20<=positionEmP and positionEmP<=24:
-                positionPointE=positionPointE+5
+                positionPointE=positionPointE+10
 
             elif 25<=positionEmP and positionEmP<=29:
-                positionPointE=positionPointE+6
+                positionPointE=positionPointE+15
+
+            elif 30<=positionEmP and positionEmP<=34:
+                positionPointE=positionPointE+100
 
             #マスの端っこは点数が低い
             if (positionEmP==0 or
@@ -61,22 +64,22 @@ def evaluateState(states):
 
 
             if 0<=positionCmP and positionCmP<=4:
-                positionPointC=positionPointC+6
+                positionPointC=positionPointC+15
 
             elif 5<=positionCmP and positionCmP<=9:
-                positionPointC=positionPointC+5
+                positionPointC=positionPointC+10
 
             elif 10<=positionCmP and positionCmP<=14:
-                positionPointC=positionPointC+4
+                positionPointC=positionPointC+6
 
             elif 15<=positionCmP and positionCmP<=19:
                 positionPointC=positionPointC+3
 
             elif 20<=positionCmP and positionCmP<=24:
-                positionPointC=positionPointC+2
+                positionPointC=positionPointC+1
 
             elif 25<=positionCmP and positionCmP<=29:
-                positionPointC=positionPointC+1
+                positionPointC=positionPointC+0
 
             #マスの端っこは点数が低い
             if (positionCmP==0 or
@@ -101,10 +104,11 @@ def evaluateState(states):
         ########相手の駒の周りについて考える##########
         #AIの駒から見てUserの駒が近くにあるか確認
         for positionE in Node.state.E_position:
+            positionEm=positionE%100#AIの駒の位置を代入（高さなし）
             for positionC in Node.state.P_position:
                 #print("AI:",positionE)
                 #print("User",positionC)
-                positionEm=positionE%100#AIの駒の位置を代入（高さなし）
+                #positionEm=positionE%100#AIの駒の位置を代入（高さなし）
                 positionCm=positionC%100#Userの駒の位置を代入（高さなし）
 
                 #近くにUserの駒があるか確認
@@ -119,7 +123,8 @@ def evaluateState(states):
                     positionEm==positionCm+4 or
                     positionEm==positionCm+5 or
                     positionEm==positionCm+6 ):
-                        isNearbyE=isNearbyE-2
+                        isNearbyE=isNearbyE-10
+
 
                 #探索木が2個目か4個目の時
                 if Node.depth%2==0:
@@ -133,27 +138,28 @@ def evaluateState(states):
                     positionEm==positionCm+4 or
                     positionEm==positionCm+5 or
                     positionEm==positionCm+6 ):
-                        isNearbyE=isNearbyE+2
+                        isNearbyE=isNearbyE+10
 
 
                 #Userの駒に乗ってたら加点する
                 if positionE+100==positionC:
-                    isNearbyE=isNearbyE+10
+                    isNearbyE=isNearbyE+15
 
                 #仲間と同じとこに行くと点数が低くなる
                 for positionEE in Node.state.E_position:
                     if positionE+100==positionEE:
-                        isNearbyE=isNearbyE-1
+                        isNearbyE=isNearbyE-4
 
 
             #print(isNearbyE)
 
         #Userの駒から見てAIの駒が近くにあるか確認
         for positionC in Node.state.P_position:
+            positionCm=positionC%100#Userの駒の位置を代入（高さなし）
             for positionE in Node.state.E_position:
 
                 positionEm=positionE%100#AIの駒の位置を代入（高さなし）
-                positionCm=positionC%100#Userの駒の位置を代入（高さなし）
+                #positionCm=positionC%100#Userの駒の位置を代入（高さなし）
 
                 #探索木が1個目か3個目か5個目のとき
                 if Node.depth%2==1:
@@ -167,7 +173,7 @@ def evaluateState(states):
                     positionCm==positionEm+4 or
                     positionCm==positionEm+5 or
                     positionCm==positionEm+6 ):
-                        isNearbyC=isNearbyC+2
+                        isNearbyC=isNearbyC-10
 
                 #探索木が2個目か4個目のとき
                 if Node.depth%2==0 :
@@ -181,11 +187,11 @@ def evaluateState(states):
                     positionCm==positionEm+4 or
                     positionCm==positionEm+5 or
                     positionCm==positionEm+6 ):
-                        isNearbyC=isNearbyC-2
+                        isNearbyC=isNearbyC+10
 
                 #AIの駒に乗ってたら加点する
                 if positionC+100==positionE:
-                    isNearbyC=isNearbyC+10
+                    isNearbyC=isNearbyC+15
 
 
         #今までの得点を合計してeval(評価点)に代入
